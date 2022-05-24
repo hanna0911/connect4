f446dea21da5296ab1c55c37c18d6ed475e53839
王昊华 <2020010942@secoder.net>
2022-05-24 10:45:07 +0800

    #2 fix bug of user 2-connect

--- Strategy.cpp
+++ Strategy.cpp
@@ -84,22 +84,24 @@ extern "C" Point *getPoint(const int M, const int N, const int *top, const int *
 	// 自己若不走一处则对方下一步有必胜策略（这里特指对方两个棋连续）
 	for(int i = N - 1; i >= 0; i--){
 		if(top[i] > 0){
-			// user走子后会具有赢面
+			// user走子后，machine阻止user其中一个赢面后user仍有一个赢面（简化版：user走子后有2个赢面）
 			x = top[i] - 1;
 			y = i;
 			board[x][y] = 1;
 			int x_2 = -1, y_2 = -1;
+			int userWinCnt = 0;
 			for(int j = N - 1; j >= 0; j--){
 				if(top[j] > 0 && i != j){ // 不考虑竖着往上摞子
 					x_2 = top[j] - 1;
 					y_2 = j;
 					board[x_2][y_2] = 1;
-					if(userWin(x_2, y_2, M, N, board)){
-						clearArray(M, N, board);
-						return new Point(x, y);
-					}
+					if(userWin(x_2, y_2, M, N, board)) userWinCnt++;
 					board[x_2][y_2] = 0;
 				}
+			}
+			if(userWinCnt > 1){
+				clearArray(M, N, board);
+				return new Point(x, y);
 			}
 			board[x][y] = 0;
 		}
