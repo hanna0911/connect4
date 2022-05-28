19abe33d14e476989fa8bf26f23e7da383eb2376
王昊华 <2020010942@secoder.net>
2022-05-29 03:33:06 +0800

    #4 test new way of time bug2

--- Strategy.cpp
+++ Strategy.cpp
@@ -383,9 +383,9 @@ Node* MCST::newNode(int player){
 Point MCST::getPoint(){
 	Node *root = newNode(2); // 2为machine
 	struct timeval currentTime;
-	gettimeofday(&currentTime, NULL);
 	double timeInterval = 0.0;
 	for(int i = 0; i < MAX_TIME; i++){
+		gettimeofday(&currentTime, NULL);
 		timeInterval = (currentTime.tv_sec - startTime.tv_sec) * 1000000 + (currentTime.tv_usec - startTime.tv_usec); // 微秒
 		if(timeInterval > 1500000) break; // 超时则停止
 		for(int i = 0; i < M; i++){
